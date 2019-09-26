83f6d211bfad2f5edb369420e3895c3152c5873d
Vincent Lepelley <v.lepelley@gmail.com>
2019-09-26 05:15:23 +0200

    Update Manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -4,15 +4,23 @@ namespace Leyzou\Mvc\Model;
 
 class Manager
 {
-    private $rdbms = 'mysql';
+    private $dns = 'mysql';
     private $host = 'localhost';
+    private $port = '3306';
     private $db = 'test';
     private $user = 'root';
     private $password = '';
 
     protected function dbConnect()
     {
-        $db = new \PDO($this->rdbms . ':host=' . $this->host . ';dbname=' . $this->db . ';charset=utf8', $this->user, $this->password);
+        if ($dns == 'mysql') { // MySQL
+            $db = new \PDO($this->dns . ':host=' . $this->host . ';dbname=' . $this->db . ';charset=utf8', $this->user, $this->password);
+        } 
+        else if ($dns == 'pgsql') { // PostgreSQL
+            $db = new \PDO($this->dns . ':host=' . $this->host . 'port=' . $port . ';dbname=' . $this->db, $this->user, $this->password);
+        }
+        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
         return $db;
     }
 }
